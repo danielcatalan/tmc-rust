2d31e6959cdcb90a28b3e868e12bff78c260c149
danielcatalan <40647262+danielcatalan@users.noreply.github.com>
2024-08-02 19:02:54 +0000

    refactor register macro

--- src/reg_macro.rs
+++ src/reg_macro.rs
@@ -1,7 +1,8 @@
 macro_rules! fields {
     ($name:ident: $t:ty, <$shift:literal>; $($rest:tt)*) => {
+
         pub fn $name(&self) -> $t {
-            ((self.0 >> $shift) & 0xFF) as u8
+            ((self.0 >> $shift) & 0x01) as u8
         }
 
         fields!($($rest)*);
@@ -11,7 +12,7 @@ macro_rules! fields {
 }
 
 macro_rules! register {
-    ($qual:vis $name:ident {$($f:tt)*}) => {
+    ($qual:vis struct $name:ident (RW) {$($f:tt)*}) => {
         $qual struct $name(u32);
 
         impl $name{
@@ -24,18 +25,29 @@ macro_rules! register {
 mod tests {
     use super::*;
     register! {
-        MyRegister {
+        struct MyRegister (RW) {
             slave_addr: u8, <0>;
-            send_delay: u8, <8>;
+            send_delay: u8, <2>;
         }
     }
 
     #[test]
     fn test_name() {
-        let x = MyRegister(0x0305);
+        let x = MyRegister(0x00);
+        assert_eq!(0, x.send_delay());
+        assert_eq!(0, x.slave_addr());
 
-        assert_eq!(3, x.send_delay());
-        assert_eq!(5, x.slave_addr());
+        let x = MyRegister(0x01);
+        assert_eq!(0, x.send_delay());
+        assert_eq!(1, x.slave_addr());
+
+        let x = MyRegister(0x04);
+        assert_eq!(1, x.send_delay());
+        assert_eq!(0, x.slave_addr());
+
+        let x = MyRegister(0x05);
+        assert_eq!(1, x.send_delay());
+        assert_eq!(1, x.slave_addr());
     }
 }
 
