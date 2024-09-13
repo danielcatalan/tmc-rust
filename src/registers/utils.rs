6468a4ec332982f7ccc90552a4c7f98405219197
danielcatalan <40647262+danielcatalan@users.noreply.github.com>
2024-09-13 22:53:24 +0000

    unit test creat_mask

--- src/registers/utils.rs
+++ src/registers/utils.rs
@@ -26,3 +26,23 @@ pub const fn create_mask(lsb: u8, msb: u8) -> u32 {
     let mask = 2u32.pow(bits as u32) - 1;
     mask << lsb
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_mask() {
+        let mask = create_mask(0,0); // 0b0001
+        assert_eq!(1, mask);
+
+        let mask = create_mask(0,1); // 0b0011
+        assert_eq!(3, mask);
+
+        let mask = create_mask(2,2); // 0b0100
+        assert_eq!(4, mask);
+
+        let mask = create_mask(0,2); // 0b0111
+        assert_eq!(7, mask);
+    }
+}
